5ea1b034ff97443ae9eb9359bf5d23f10cfb6942
Matas Urbonavicius <79410740+matasurbonavicius@users.noreply.github.com>
2023-10-16 20:47:11 +0300

    Update Black_Scholes_model.cpp

--- Cpp/black_scholes_model.cpp
+++ Cpp/black_scholes_model.cpp
@@ -1,35 +1,45 @@
 #include <iostream>
-#include <limits>
 #include <cmath>
-#include <fstream>
-#include <windows.h>
-#include <algorithm>
-#include <vector>
-#include <iomanip> 
+#include <iomanip>
 
+class BlackScholesOption {
+private:
+    double S, K, r, T, sigma;
+    double d1, d2;
 
-double N(double x) {
-    return 0.5 * erfc(-x * std::sqrt(0.5));
-}
+    void computeD1D2() {
+        d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * T) / (sigma * sqrt(T));
+        d2 = d1 - sigma * sqrt(T);
+    }
 
-double blackScholesCall(double S, double K, double r, double T, double sigma) {
-    double d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * T) / (sigma * sqrt(T));
-    double d2 = d1 - sigma * sqrt(T);
-    return S * N(d1) - K * exp(-r * T) * N(d2);
-}
+    double N(double x) const {
+        return 0.5 * erfc(-x * std::sqrt(0.5));
+    }
 
-double blackScholesPut(double S, double K, double r, double T, double sigma) {
-    double d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * T) / (sigma * sqrt(T));
-    double d2 = d1 - sigma * sqrt(T);
-    return K * exp(-r * T) * N(-d2) - S * N(-d1);
-}
+public:
+    BlackScholesOption(double S, double K, double r, double T, double sigma)
+        : S(S), K(K), r(r), T(T), sigma(sigma) {
+        computeD1D2();
+    }
+
+    double callPrice() const {
+        return S * N(d1) - K * exp(-r * T) * N(d2);
+    }
+
+    double putPrice() const {
+        return K * exp(-r * T) * N(-d2) - S * N(-d1);
+    }
+
+    double callDelta() const {
+        return N(d1);
+    }
+
+    double putDelta() const {
+        return N(d1) - 1;
+    }
+};
 
 int main() {
-    double S;
-    double K;
-    double r;
-    double t;      // In days
-    double sigma;  // Volatility annualized
 
     /*
     Mathematical notations:
@@ -40,12 +50,14 @@ int main() {
     r = Risk-free interest rate
     t = Time to maturity
     N = Normal Distribution
-    
+
     */
 
-    char option;
+    double S, K, r, t, sigma;
+
+    char optionType;
     std::cout << "What is the option? (Enter C for call & P for put) - ";
-    std::cin >> option;
+    std::cin >> optionType;
     std::cout << "What is the current stock price? - ";
     std::cin >> S;
     std::cout << "What is the strike price? - ";
@@ -57,22 +69,25 @@ int main() {
     std::cout << "What is the annualized volatility? (if 10%, then enter 0.1) - ";
     std::cin >> sigma;
 
-    double T = t / 365; // Convert days to years
-    double optionPrice;
+    double T = t / 365;  // Convert days to years
 
-    if (option == 'C' || option == 'c') {
-        optionPrice = blackScholesCall(S, K, r, T, sigma);
-    }
-    else if (option == 'P' || option == 'p') {
-        optionPrice = blackScholesPut(S, K, r, T, sigma);
-    }
-    else {
-        optionPrice = 0.0;
-    }
+    BlackScholesOption option(S, K, r, T, sigma);
 
     std::cout << std::fixed << std::setprecision(2);
-    std::cout << "The theoretical option price is: $" << optionPrice << std::endl;
+    if (optionType == 'C' || optionType == 'c') {
+        std::cout << "The theoretical call option price is: $" << option.callPrice() << std::endl;
+        std::cout << "Delta for the call option is: " << option.callDelta() << std::endl;
+    }
+    else if (optionType == 'P' || optionType == 'p') {
+        std::cout << "The theoretical put option price is: $" << option.putPrice() << std::endl;
+        std::cout << "Delta for the put option is: " << option.putDelta() << std::endl;
+    }
+    else {
+        std::cout << "Invalid option type entered." << std::endl;
+    }
 
     return 0;
 }
+
+
 
