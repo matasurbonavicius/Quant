9320952b1a79217b52e419dc3a2d7764e6f628dc
Matas Urbonavicius <matas.urbonavicius@gmail.com>
2023-10-20 21:07:09 +0300

    Upload Options Pricing & Evaluation Model

---
+++ Cpp/Options/Definitions.h
@@ -0,0 +1,21 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+// Define an Option struct to encapsulate details about an option.
+struct OptionDetails {
+    int type;                      // 1 for call, 0 for put
+    int quantity;                  // negative for selling
+    double impliedPremium; 
+    double underlying;             // Market mid price
+    double stockPrice;
+    double strikePrice;
+    double riskFreeRate;
+    double daysTillMaturity;
+    double theoreticalPremium;     // Model calculated price
+    double annualizedVolatility;
+};
+
+double callInstristicValue(double stockPrice, double strikePrice);
+double putInstristicValue(double stockPrice, double strikePrice);
+
+#endif
